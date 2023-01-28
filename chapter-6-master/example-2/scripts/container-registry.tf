46f2112bcb83d6067f5a27e2e94efc9cdf1647ef
kikikrittamate <kikikrittamate@gmail.com>
2023-01-28 14:53:58 +0700

    change name

--- chapter-6-master/example-2/scripts/container-registry.tf
+++ chapter-6-master/example-2/scripts/container-registry.tf
@@ -1,7 +1,7 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "flixtube"
+  name                = "kikikrittamate"
   resource_group_name = azurerm_resource_group.flixtube.name
   location            = "westus"
   admin_enabled       = true
@@ -18,4 +18,5 @@ output "registry_un" {
 
 output "registry_pw" {
   value = azurerm_container_registry.container_registry.admin_password
+  sensitive = true
 }
