ba9e1c956ac168b571d1446eec39fe5ac45c2e45
kikikrittamate <kikikrittamate@gmail.com>
2023-01-28 14:44:16 +0700

    fix bugs in ex2

--- chapter-6-master/example-2/scripts/providers.tf
+++ chapter-6-master/example-2/scripts/providers.tf
@@ -1,5 +1,14 @@
 # Initialises Terraform providers and sets their version numbers.
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    version = "1.38.0"
+  features {}
 }
